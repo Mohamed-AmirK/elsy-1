a18c5af6d14d1e2b7f92c4d645d6ce291b821eff
Mohamed-Amir kharmaz <amirpro@hotmail.fr>
2021-08-18 17:30:25 +0200

    First commit Elsy2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React from "react"
 import Box from './components/Box'
 import './style/global.css'
+
 
 const tempMin = -20
 const tempMax = 40
@@ -10,15 +11,32 @@ const stepsMin = 0
 const stepsMax = 50000
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props)
+    this.state = {
+      water: 0,
+      heart: 120,
+      temperature: -10,
+      steps: 3000
+    }
+  }
+
+      onHeartChange= (e) =>{
+        console.log(e.target.value)
+        this.setState({heart: e.target.value})        
+      }
+
+  render() { 
     return (
       <div className="container-fluid">
         <div className="row">
-          <p>Heart : {heartMin}</p>
-          <p>Temperature : {tempMin}</p>
-          <p>Steps : {stepsMin}</p>
-        </div>
 
+          {/* <p>Heart : {heartMin}</p>
+          <p>Temperature : {tempMin}</p>
+          <p>Steps : {stepsMin}</p> */}
+
+        </div>
+        
         <div className="row">
           {/* Water */}
           <Box icon="local_drink" color="#3A85FF" value={1.5} unit="L" />
@@ -27,7 +45,7 @@ class App extends React.Component {
           <Box icon="directions_walk" color="black" value={3000} unit="steps" />
 
           {/* Heart */}
-          <Box icon="favorite" color="red" value={120} unit="bpm" />
+          <Box icon="favorite" color="red" value={this.state.heart} unit="bpm" min={heartMin} max={heartMax} onChange={this.onHeartChange}/>
 
           {/* Temperature */}
           <Box icon="wb_sunny" color="yellow" value={-10} unit="°C" />
--- src/components/Box.js
+++ src/components/Box.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 class Box extends React.Component {
   render() {
+        console.log(this.props)
     return (
       <div className="box col-sm-3 col-6">
         <span
@@ -10,6 +11,9 @@ class Box extends React.Component {
           {this.props.icon}
         </span>
         <p>{this.props.value} {this.props.unit}</p>
+        {"local_drink" !== this.props.icon &&
+          <input type="range" min={this.props.min} max={this.props.max} value={this.props.value} onChange={this.props.onChange}></input>
+        }
       </div>
     )
   }
